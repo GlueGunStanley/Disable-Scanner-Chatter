e55dfe81d0ccba68fe5fad6fa25daa2dc41de94d
GlueGunStanley <kasonstanley@gmail.com>
2024-12-09 15:37:37 -0500

    Updated LICENSE and README.md

---
+++ Disable-Scanner-Chatter/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'Stanley Development Studios'
+description 'Disables the scanner chatter that plays while driving an emergency vehicle'
+
+github 'https://github.com/GlueGunStanley/Disable-Flight-Music'
+discord 'https://discord.com/invite/uCKZJed3Gq'
+
+client_script 'client/cl_main.lua'
